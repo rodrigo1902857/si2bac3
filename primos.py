899cfd6eb3b006fba93047ad9ffd916f98de2079
unknown <rodrigo.lourenco@aluno.faculdadeimpacta.com.br>
2020-10-08 19:28:39 -0300

    primos fix

--- primos.py
+++ primos.py
@@ -6,26 +6,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def nao_entre_em_panico():
-    limite=100
-    c=1
-    p=1
+
+
+    limite = 100
+    c = 1
+    p = 1
     numero = 3
+
     primos = "2,"
 
     while p < limite:
-        ehprimo=1
+        ehprimo = 1
         for i in range(2, numero):
             if numero % i == 0:
                 ehprimo = 0
                 break
-        if (ehprimo):
+        if(ehprimo):
             primos = primos + str(numero) + ","
-            p +=1
-            if (p%10==0):
-                primos=primos + "<br>"
-            numero+=1
-
-
+            p += 1
+            if(p % 10 == 0):
+                primos = primos + "<br>"
+        numero += 1
     return primos
 
 if __name__ == "__main__":
